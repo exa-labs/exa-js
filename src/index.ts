ecc5911bfc63c684f9a217bb0e0bed8b1f8146b8
jeffzwang <jeffreyzhiyuanwang@gmail.com>
2023-07-27 20:17:26 -0700

    change type for score to be potentiall undefined

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export interface Result {
   url: string; // The URL of the search result.
   publishedDate?: string; // The estimated creation date of the content. Format is YYYY-MM-DD. Nullable
   author?: string; // The author of the content, if available. Nullable
-  score: number; // A number from 0 to 1 representing similarity between the query/url and the result.
+  score?: number; // A number from 0 to 1 representing similarity between the query/url and the result.
   id: string; // The temporary ID for the document. Useful for /contents endpoint.
 }
 
