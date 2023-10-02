d62a251766480b945ef2fcf4c1ac0439cd3c7c39
jiito <benjamin.allanrahill@gmail.com>
2023-10-01 21:37:55 -0700

    fix: versioning

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -67,7 +67,7 @@ export default class Metaphor {
     this.headers = new Headers({
       'x-api-key': apiKey,
       'Content-Type': 'application/json',
-      'User-Agent': 'metaphor-node 1.0.19',
+      'User-Agent': 'metaphor-node 1.0.20',
     });
   }
 
