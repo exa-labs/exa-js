4041389c315c1b1db2835478aeafde626a310980
jiito <benjamin.allanrahill@gmail.com>
2023-10-01 18:39:54 -0700

    cleanup example

--- examples/node/server.ts
+++ examples/node/server.ts
@@ -6,10 +6,12 @@ const metaphorClient = new Metaphor(process.env.METAPHOR_API_KEY!);
 const app: Express = express();
 const port = 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  const mres = metaphorClient.search("hottest ai startup");
-  console.log(mres);
-  res.send("Express + TypeScript Server");
+app.get("/", async (req: Request, res: Response) => {
+  const metaphorRes = await metaphorClient.search(
+    "If you're looking for the hottest AI agent startup, check this out:"
+  );
+
+  res.json(metaphorRes);
 });
 
 app.listen(port, () => {
