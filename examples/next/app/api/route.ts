e820d7a827b78bb09066fda489a66a70394a83c5
jiito <benjamin.allanrahill@gmail.com>
2023-10-03 13:12:29 -0700

    fix: route api key and package

--- examples/next/app/api/route.ts
+++ examples/next/app/api/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
-import Metaphor from "jiito-kowalski-node";
+import Metaphor from "metaphor-node";
 
 export async function GET(req: NextResponse) {
-  const metaphor = new Metaphor("fcac2ebb-a2fe-4348-bf45-7470298f0055");
+  const metaphor = new Metaphor(process.env.METAPHOR_API_KEY!);
   const res = await metaphor.search("hottest ai startups");
 
   return NextResponse.json(res);
