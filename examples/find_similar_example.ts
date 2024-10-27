a57bf73a3056459cd51ef4256258c36eab8573af
Felix Zeller <felixazeller@gmail.com>
2024-10-26 18:15:24 -0700

    update workflow

--- examples/find_similar_example.ts
+++ examples/find_similar_example.ts
@@ -9,7 +9,8 @@ async function runFindSimilarExamples() {
     console.log("Find similar results:", findSimilarResponse.results);
 
     // Find similar with contents
-    const findSimilarWithContentsResponse = await exa.findSimilarAndContents("https://www.example.com", { text: true });
+    const findSimilarWithContentsResponse = await exa.findSimilarAndContents("https://www.example.com", { summary: true });
+    findSimilarResponse.results.map(it => it.text)
     console.log("Find similar results with contents:", findSimilarWithContentsResponse.results);
   } catch (error) {
     console.error("Error in find similar examples:", error);
