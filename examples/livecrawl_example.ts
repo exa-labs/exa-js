2ef23adbc97b72594202cfa723514274adcd9ed3
Felix Zeller <felixazeller@gmail.com>
2024-10-28 13:59:08 -0700

    fix examples

--- examples/livecrawl_example.ts
+++ examples/livecrawl_example.ts
@@ -1,7 +1,5 @@
 import Exa from "../src/index";
 
-import assert from "assert"
-
 const exa = new Exa(process.env.EXASEARCH_API_KEY);
 
 async function runLivecrawlExample() {
@@ -12,7 +10,6 @@ async function runLivecrawlExample() {
       text: true
     });
     console.log("Get contents results without livecrawl:", JSON.stringify(contentsWithoutLivecrawl, null, 2));
-    assert(contentsWithoutLivecrawl.results[0].text, "Text content is missing from get contents results without livecrawl");
 
     // Get contents with livecrawl
     const contentsWithLivecrawl = await exa.getContents("tesla.com", { 
@@ -20,14 +17,16 @@ async function runLivecrawlExample() {
       text: true
     });
     console.log("Get contents results with livecrawl:", JSON.stringify(contentsWithLivecrawl, null, 2));
-    assert(contentsWithLivecrawl.results[0].text, "Text content is missing from get contents results with livecrawl");
 
     // Compare the results
     const textWithoutLivecrawl = contentsWithoutLivecrawl.results[0].text;
     const textWithLivecrawl = contentsWithLivecrawl.results[0].text;
 
-    assert(textWithoutLivecrawl !== textWithLivecrawl, "The content with and without livecrawl should be different");
-    console.log("Livecrawl comparison successful: The content with and without livecrawl is different.");
+    if (textWithoutLivecrawl !== textWithLivecrawl) {
+      console.log("Livecrawl comparison successful: The content with and without livecrawl is different.");
+    } else {
+      console.log("Livecrawl comparison note: The content with and without livecrawl is the same.");
+    }
 
     // Log the lengths of the texts for comparison
     console.log("Length of text without livecrawl:", textWithoutLivecrawl.length);
--- examples/subpages.ts
+++ examples/subpages.ts
@@ -1,7 +1,5 @@
 import Exa from "../src/index";
 
-import assert from "assert";
-
 const exa = new Exa(process.env.EXASEARCH_API_KEY);
 
 async function runExamples() {
@@ -12,7 +10,6 @@ async function runExamples() {
       numResults: 1
     });
     console.log("Search results with subpages:", JSON.stringify(search, null, 2));
-    assert(search.results[0].subpages && search.results[0].subpages.length > 0, "Subpages array is empty for search results");
 
     // Find similar with subpages
     const similar = await exa.findSimilarAndContents("https://www.tesla.com/models", { 
@@ -20,7 +17,6 @@ async function runExamples() {
       numResults: 1
     });
     console.log("Find similar results with subpages:", JSON.stringify(similar, null, 2));
-    assert(similar.results[0].subpages && similar.results[0].subpages.length > 0, "Subpages array is empty for similar results");
 
     // Get contents with subpages
     if (search.results.length > 0) {
@@ -29,7 +25,6 @@ async function runExamples() {
         text: true
       });
       console.log("Get contents results with subpages:", JSON.stringify(contents, null, 2));
-      assert(contents.results[0].subpages && contents.results[0].subpages.length > 0, "Subpages array is empty for contents results");
     }
 
   } catch (error) {
